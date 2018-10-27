97925567b3b0beb35add50296986fc13f9810acf
NOLLY DOINOG <41631362+nollydoinog@users.noreply.github.com>
2018-10-27 23:58:51 +0800

    Update PROG_ASS5.cpp

--- PROG_ASS5.cpp
+++ PROG_ASS5.cpp
@@ -1,5 +1,4 @@
 #include <iostream>                 
-#include <string>
 using namespace std;
 
 int main()
@@ -19,7 +18,19 @@ int main()
      
      for (int x=1;x<5;x++)
      {
-       cout<<"5:00 - PRAYERTIME"<<endl;
+       cout<<"5:00 am - 5:30 am ==> PRAYERTIME"<<endl;
+       for (int y=0;y<1;y--)
+       {
+           cout<<"5:30 am - 6:30 am ==> LIGO`LIGO na "<<endl;
+           for (int z=0;z<1;z--)
+           {
+                cout<<"6:30 am - 7:00 am ==> BREAKFAST"<<endl;
+                for (int a=0;a<1;a--)
+                    {
+                        cout<<"7:00 am - 7:30 am ==> ON THE WAY TO SCHOOL"<<endl;
+                    }
+           }
+       }
      }
 
 
